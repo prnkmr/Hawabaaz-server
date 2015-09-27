def1765bbf4ab0e239d8ddeaf165705e2f9c5b84
Praveen kumar <praveen.k9894@gmail.com>
2015-09-27 19:30:02 +0530

    Library Update

--- listlocations.php
+++ listlocations.php
@@ -1,19 +1,19 @@
 <?php
 require_once("praveen.php");
 $app=new praveen();
-if(debug)$respjson[status]="unprocessed";
+if(debug)$resp[status]="unprocessed";
 $con=$app->getConnection();
 $sql="select id,name from hawabaaz.available_locations";
  $result=$app->query($sql);
    $resp["list"]=array();
      while($row=$result->fetch_array()){
          $entry=array($row['id'],$row['name']);
-         $respjson["list"][]=$entry;
+         $resp["list"][]=$entry;
 
      }
-     $respjson[error]=0;
+     $resp[error]=0;
      if($sk->debug) {
-         $respjson['status'] = "success";
+         $resp['status'] = "success";
      }
-echo json_encode($respjson);
+echo json_encode($resp);
 ?>
